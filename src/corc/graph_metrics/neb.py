working on NEB and fixing the wrong distance computation (#35)
* computing ari scores for the cached embeddings

* mainly fixing the wrong computation in tmm_gmm_neb.py (moving stuff out of the loop)

* repairing small plotting things

* comments and minor changes in reply to PR review

* deleted ari_scores from git history

---------

Co-authored-by: Polina Turishcheva <49938348+pollytur@users.noreply.github.com>

src/corc/graph_metrics/neb.py
@@ -16,7 +16,7 @@ import corc.graph_metrics.tmm_gmm_neb
 class NEB(Graph):
     def __init__(
         self,
-        latent_dim,
+        latent_dim=2,
         data=None,
         labels=None,
         path=None,
@@ -26,7 +26,8 @@ class NEB(Graph):
         seed=42,
         mixture_model_type="tmm",
         n_init=5,
-        optimization_iterations=1000,
+        optimization_iterations=1000, # for NEB
+        max_iter_on_retries=10000, # for TMM fitting, 10x the default
         dataset_name=None,
     ):
         """
@@ -41,12 +42,16 @@ class NEB(Graph):
             n_init (int): Number of repetitions during GMM/TMM fitting.
             mixture_model_type (str): GMM or TMM
         """
+        # make sure that latent_dim does match the data if provided
+        if data is not None:
+            latent_dim = data.shape[-1]
+
         super().__init__(latent_dim, data, labels, path, seed)
 
         if mixture_model_type == "tmm":
             self.mixture_model = studenttmixture.EMStudentMixture(
                 n_components=n_components,
-                n_init=n_init,
+                n_init=1, # note that in the fit function we give it more tries.
                 fixed_df=False,
                 # df=1.0,  # the minimum value, for df=infty we get gmm
                 init_type="k++",
@@ -66,22 +71,34 @@ class NEB(Graph):
         )
         self.thresh = thresh
         self.iterations = optimization_iterations
+        self.n_init = n_init
+        self.max_iter_on_retries = max_iter_on_retries
 
-    def fit(self, data):
-        self.data = data
+
+    def fit(self, data, max_elongation=100):
+        # data: data to be fitted on.
+        # max_elongation: for TMM ignore all components that are highly elongated.
+        # max_elongation gives an upper bound on biggest_eigenvalue/smallest_eigenvalue
         self.mixture_model.fit(data)
 
         # extract centers for TMM/GMM
         if isinstance(self.mixture_model, sklearn.mixture.GaussianMixture):
             self.centers_ = self.mixture_model.means_
         elif isinstance(self.mixture_model, studenttmixture.EMStudentMixture):
-            for _ in range(20):
+            # retry fitting if it failed before - happens regularly for high-dim datasets
+            for _ in range(self.n_init):
                 if self.mixture_model.df_ is None:
                     # we have not converged
                     print("retrying tmm fit with more iterations")
-                    self.mixture_model.max_iter = 10000
+                    self.mixture_model.max_iter = self.max_iter_on_retries
                     self.mixture_model.fit(data)
+
+            # filter elongated components
+            original_num_components = self.mixture_model.n_components
+            self.mixture_model = self.filter_mixture_model(self.mixture_model)
             self.centers_ = self.mixture_model.location
+            print(f"After filtering {original_num_components} components, we are left with {self.mixture_model.n_components} components")
+
 
         # compute NEB paths. This is a very time-consuming step
         (
@@ -93,6 +110,14 @@ class NEB(Graph):
         ) = corc.graph_metrics.tmm_gmm_neb.compute_neb_paths(
             self.mixture_model, iterations=self.iterations
         )
+
+        # check quality of generated paths
+        equidistance_factor, all_factors = corc.graph_metrics.tmm_gmm_neb.evaluate_equidistance(self.paths_)
+        if equidistance_factor>10:
+            print(f"WARNING: the path is not equidistant! longest segment {equidistance_factor} times too long")
+            for edge in all_factors.keys():
+                if all_factors[edge]>10:
+                    print(f"{edge} has factor {all_factors[edge]}")
 
         # normalize adjacency
         norm_adjacency = self.adjacency_ - np.min(self.adjacency_)
@@ -281,3 +306,25 @@ class NEB(Graph):
                 color="black",
                 # color=cmap(normalized_component_labels[i]),
             )
+
+    def filter_mixture_model(self, mixture_model, max_elongation=100):
+        # compute cluster elongations
+        elongations = []
+        for i in range(mixture_model.scale_.shape[2]):
+            eigenvalues = np.linalg.eigvalsh(mixture_model.scale_[:, :, i])
+            elongation = max(eigenvalues) / min(eigenvalues)
+            elongations.append(elongation)
+
+        # create filter to remove elongated components
+        elongation_filter = np.array(elongations) < max_elongation
+
+        # remove components that are too elongated
+        mixture_model.location_ = mixture_model.location_[elongation_filter]
+        mixture_model.scale_ = mixture_model.scale_[:, :, elongation_filter]
+        mixture_model.n_components = np.count_nonzero(elongation_filter)
+        mixture_model.mix_weights_ = mixture_model.mix_weights_[elongation_filter]
+        mixture_model.df_ = mixture_model.df_[elongation_filter]
+        mixture_model.scale_inv_cholesky_ = mixture_model.scale_inv_cholesky_[:, :, elongation_filter]
+        mixture_model.scale_cholesky = mixture_model.scale_cholesky_[:, :, elongation_filter]
+
+        return mixture_model

src/corc/graph_metrics/tmm_gmm_neb.py
@@ -167,7 +167,8 @@ def compute_interpolation(
         ms = optax.apply_updates(ms, updates)
 
         # the following line effectively resamples the line through linear interpolation. Without it, all of the 1000 points will be pushed away from the low-density regions instead of finding the best possible line between the two dense regions.
-        ms = scipy.signal.savgol_filter(ms, window_length=100, polyorder=1, axis=0)
+        # TODO: this does not work!!!
+        ms = scipy.signal.savgol_filter(ms, window_length=100, polyorder=5, axis=0)
 
     # this is where the band becomes elastic and where the work is done.
     # for _ in range(iterations):
@@ -236,18 +237,16 @@ def compute_neb_paths(mixture_model, iterations=1000):
         # evaluate score of elastic band (minimum value along the path)
         adjacency[i, j] = adjacency[j, i] = min(interpolation_probs)
 
-        raw_adjacency = adjacency.copy()
-        adjacency = compute_mst_distances(raw_adjacency)
+    raw_adjacency = adjacency.copy()
+    adjacency = compute_mst_distances(raw_adjacency)
 
     return adjacency, raw_adjacency, paths, temps, logprobs
 
-
-""" Takes a distance matrix from NEB, computes an MST, and outputs a corrected distance 
-especially useful in cases where NEB did not fully converge for all paths. This is already used in compute_neb_paths
-"""
-
-
 def compute_mst_distances(adjacency):
+    """
+    Takes a distance matrix from NEB, computes an MST, and outputs a corrected distance
+    especially useful in cases where NEB did not fully converge for all paths. This is already used in compute_neb_paths
+    """
     mst = -scipy.sparse.csgraph.minimum_spanning_tree(-adjacency)
     num_nodes = adjacency.shape[0]
 
@@ -280,9 +279,10 @@ def compute_mst_distances(adjacency):
                     distances[predecessors[node]][other_node],
                 )  # the latter is already computed and the first has a value
                 # assign to both directions
-                distances[node][other_node] = distances[other_node][node] = max(
+                distances[node][other_node] = max(
                     direct_dist, indirect_dist
                 )
+                distances[other_node][node] = distances[node][other_node]
 
         marked[node] = 1
 
@@ -310,3 +310,37 @@ def compute_mst_distances(adjacency):
 #     cluster_numbers = np.array(cluster_numbers)
 #
 #     return thresholds, cluster_numbers, clusterings
+
+def evaluate_equidistance(paths):
+    """
+    looks at distances between points in paths.
+    First computes the longest path and the corresponding granularity (path_length/num_points).
+    Then compares all path-segments and computes the worst factor (segment/granularity) in each path.
+    returns the worst overall factor and a dictionary with factors for all paths where a segment is longer than the granularity.
+    """
+    worst_factor = 1
+
+    # first compute the "general granularity" (to ignore inconsistencies below that)
+    longest_equidistant_length = 0
+    for path_index in paths:
+        path = paths[path_index]
+        distances = np.linalg.norm(np.diff(path, axis=0), axis=1)
+        total_length = np.sum(distances)
+        equidistant_length = total_length / len(distances)
+        if equidistant_length > longest_equidistant_length:
+            longest_equidistant_length = equidistant_length
+
+    # go through all paths and check whether their segments are equidistant
+    all_factors = dict()
+    for path_index in paths:
+        path = paths[path_index]
+        distances = np.linalg.norm(np.diff(path, axis=0), axis=1)
+        longest_segment = max(distances)
+        total_length = np.sum(distances)
+        equidistant_length = total_length/len(distances)
+        if longest_segment > longest_equidistant_length:
+            factor = longest_segment / longest_equidistant_length
+            all_factors[path_index] = factor
+            if factor > worst_factor:
+                worst_factor = factor
+    return worst_factor, all_factors

src/corc/tmm_plots.py
@@ -5,8 +5,10 @@ import scipy
 import studenttmixture
 import sklearn
 
-import corc.graph_metrics.tmm_gmm_neb as jax_neb
+import corc.graph_metrics.neb
+from corc.graph_metrics import tmm_gmm_neb
 
+GRID_RESOLUTION = 128 # the resolution for the "heatmap" computation for TMM plotting
 
 def plot_logprob_lines(mixture_model, i, j, temps, logprobs, path=None):
     """plotting probabilities between the direct line and the nudged elastic band"""
@@ -51,12 +53,12 @@ def plot_field(
     n_components = len(locations)
 
     # grid coordinates
-    x = np.linspace(data_X[:, 0].min() - 0.1, data_X[:, 0].max() + 0.1, 128)
-    y = np.linspace(data_X[:, 1].min() - 0.1, data_X[:, 1].max() + 0.1, 128)
+    x = np.linspace(data_X[:, 0].min() - 0.1, data_X[:, 0].max() + 0.1, GRID_RESOLUTION)
+    y = np.linspace(data_X[:, 1].min() - 0.1, data_X[:, 1].max() + 0.1, GRID_RESOLUTION)
     XY = np.stack(np.meshgrid(x, y), -1)
 
     # get scores for the grid values
-    mm_probs = mixture_model.score_samples(XY.reshape(-1, 2)).reshape(128, 128)
+    mm_probs = mixture_model.score_samples(XY.reshape(-1, 2)).reshape(GRID_RESOLUTION, GRID_RESOLUTION)
 
     if axis is None:
         figure, axis = plt.subplots(1, 1)
@@ -124,13 +126,13 @@ def computations_for_plot_row(
     model.fit(np.array(data_X, dtype=np.float64))
 
     # compute elastic band paths
-    adjacency, raw_adjacency, paths, temps, logprobs = jax_neb.compute_neb_paths(
+    adjacency, raw_adjacency, paths, temps, logprobs = tmm_gmm_neb.compute_neb_paths(
         model, iterations=iterations
     )
 
     # thresholds for the cluster-number plot
     thresholds, cluster_numbers, clusterings = (
-        jax_neb.get_thresholds_and_cluster_numbers(adjacency)
+        tmm_gmm_neb.get_thresholds_and_cluster_numbers(adjacency)
     )
 
     # extracting the smallest edges to only draw them in the heatmap
@@ -148,21 +150,39 @@ def plot_row_with_computation(
     mixture_model="tmm",
 ):
     # main computation
-    mixture_model, adjacency, paths, cluster_numbers, mst_edges, clusterings = (
-        computations_for_plot_row(
-            data_X, overclustering_n, iterations=iterations, mixture_model=mixture_model
-        )
+    tmm_model = corc.graph_metrics.neb.NEB(
+        latent_dim=data_X.shape[1],
+        data=data_X,
+        labels=data_y,
+        optimization_iterations=iterations,
+        mixture_model_type=mixture_model,
     )
+    # this is the time-consuming step
+    tmm_model.fit(data=data_X)
 
-    plot_row(data_X, data_y, mixture_model, paths, cluster_numbers, mst_edges)
+    plot_row(
+        data_X=data_X,
+        data_y=data_y,
+        tmm_model=tmm_model,
+    )
 
     if levels is None:
         target_cluster_n = len(np.unique(data_y))
         levels = [target_cluster_n - 1, target_cluster_n, target_cluster_n + 1]
-    plot_cluster_levels(levels, mixture_model, data_X, adjacency, paths)
+    plot_cluster_levels(levels, tmm_model, data_X)
 
 
-def plot_row(data_X, data_y, mixture_model, paths, cluster_numbers, mst_edges):
+def plot_row(
+        data_X,
+        data_y,
+        tmm_model,
+):
+    """
+    Creates a plot consisting of
+    1) the GT clustering,
+    2) the TMM clustering including the heatmap and all MST edges - no joining of clusters
+    3) threshold values against number of clusters
+    """
     fig, axes = plt.subplots(1, 3, figsize=(16, 6))
 
     # ground truth
@@ -170,7 +190,8 @@ def plot_row(data_X, data_y, mixture_model, paths, cluster_numbers, mst_edges):
     axes[0].set_title("Ground Truth")
 
     # heatmap with arrows
-    plot_field(data_X, mixture_model, paths=paths, selection=mst_edges, axis=axes[1])
+    mst_edges = tmm_model.compute_mst_edges(tmm_model.raw_adjacency_)
+    plot_field(data_X, tmm_model.mixture_model, paths=tmm_model.paths_, selection=mst_edges, axis=axes[1])
     axes[1].set_title("Heatmap")
 
     # # threshold counts
@@ -179,26 +200,31 @@ def plot_row(data_X, data_y, mixture_model, paths, cluster_numbers, mst_edges):
     # axes[2].set_xlabel('Threshold')
 
     # clusters vs thresholds
-    axes[2].plot(cluster_numbers[:, 1], cluster_numbers[:, 0], marker="o")
+    thresholds, cluster_numbers_per_threshold, clusterings = tmm_model.get_thresholds_and_cluster_numbers()
+    axes[2].plot(cluster_numbers_per_threshold[:, 1], cluster_numbers_per_threshold[:, 0], marker="o")
+    axes[2].axvline(x=len(np.unique(data_y)), color='r')
     axes[2].set_xlabel("Number of clusters")
     axes[2].set_ylabel("Threshold")
     axes[2].set_title("Clusters")
     axes[2].grid()
 
 
-def plot_cluster_levels(levels, tmm, data_X, adjacency, paths, save_path=None):
+
+def plot_cluster_levels(levels, tmm_model, data_X, save_path=None):
+    """
+    creates a 1-line plot of the tmm clustering such but with different target_number_cluster, so with different merging strategies.
+    levels: a list of integers of how many clusters should be in each plot, its length determines the size of the plot.
+    """
     n_plots = len(levels)
 
     fig, axes = plt.subplots(1, n_plots, figsize=(n_plots * 4, 6))
-    thresholds, cluster_numbers, counts = jax_neb.get_thresholds_and_cluster_numbers(
-        adjacency
-    )
+    thresholds, cluster_numbers, counts = tmm_model.get_thresholds_and_cluster_numbers()
 
     # grid coordinates
-    x = np.linspace(data_X[:, 0].min() - 0.1, data_X[:, 0].max() + 0.1, 128)
-    y = np.linspace(data_X[:, 1].min() - 0.1, data_X[:, 1].max() + 0.1, 128)
+    x = np.linspace(data_X[:, 0].min() - 0.1, data_X[:, 0].max() + 0.1, GRID_RESOLUTION)
+    y = np.linspace(data_X[:, 1].min() - 0.1, data_X[:, 1].max() + 0.1, GRID_RESOLUTION)
     XY = np.stack(np.meshgrid(x, y), -1)
-    tmm_probs = tmm.score_samples(XY.reshape(-1, 2)).reshape(128, 128)
+    tmm_probs = tmm_model.mixture_model.score_samples(XY.reshape(-1, 2)).reshape(GRID_RESOLUTION, GRID_RESOLUTION)
 
     cmap = plt.get_cmap("viridis")  # choose a colormap
 
@@ -212,7 +238,7 @@ def plot_cluster_levels(levels, tmm, data_X, adjacency, paths, save_path=None):
         level_index = np.where(cluster_numbers == level)[0][0]
         threshold = thresholds[level_index]
         # all pairs that are merged (i.e. are below threshold)
-        tmp_adj = np.array(adjacency >= threshold, dtype=int)
+        tmp_adj = np.array(tmm_model.adjacency_ >= threshold, dtype=int)
         pairs = np.transpose(np.nonzero(tmp_adj))
         _, component_labels = scipy.sparse.csgraph.connected_components(
             tmp_adj, directed=False
@@ -225,8 +251,8 @@ def plot_cluster_levels(levels, tmm, data_X, adjacency, paths, save_path=None):
         axis.contourf(x, y, tmm_probs, levels=20, cmap="coolwarm", alpha=0.5)
         axis.scatter(data_X[:, 0], data_X[:, 1], s=10, label="raw data")
         axis.scatter(
-            tmm.location[:, 0],
-            tmm.location[:, 1],
+            tmm_model.mixture_model.location[:, 0],
+            tmm_model.mixture_model.location[:, 1],
             c=component_labels,
             cmap="viridis",
             marker="X",
@@ -237,7 +263,7 @@ def plot_cluster_levels(levels, tmm, data_X, adjacency, paths, save_path=None):
         for i, j in pairs:
             if i == j:
                 continue
-            path = paths[(i, j)]
+            path = tmm_model.paths_[(i, j)]
             axis.plot(
                 path[:, 0],
                 path[:, 1],
@@ -246,7 +272,7 @@ def plot_cluster_levels(levels, tmm, data_X, adjacency, paths, save_path=None):
                 color=cmap(normalized_component_labels[i]),
             )
 
-        axis.set_title(f"{level} target clusters ({len(tmm.location)} total)")
+        axis.set_title(f"{level} target clusters ({len(tmm_model.mixture_model.location)} total)")
 
     if save_path is not None:
         plt.savefig(save_path)